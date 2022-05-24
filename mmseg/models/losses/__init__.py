d3a8ed7c9f5b67038f7fa2151c21d87b842e4b68
unknown <226214208@qq.com>
2022-05-24 17:57:14 +0800

    add focal loss codes copied from mmseg

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -6,9 +6,10 @@ from .dice_loss import DiceLoss
 from .smp_dice_loss import SMPDiceLoss
 from .lovasz_loss import LovaszLoss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
+from .focal_loss import FocalLoss
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
-    'weight_reduce_loss', 'weighted_loss', 'LovaszLoss', 'DiceLoss', 'SMPDiceLoss'
+    'weight_reduce_loss', 'weighted_loss', 'LovaszLoss', 'DiceLoss', 'SMPDiceLoss', 'FocalLoss'
 ]
